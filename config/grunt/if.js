d9740af99caa43a4d9efb77c69209637eac28e8f
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-13 02:27:09 +0200

    change test for tags

--- config/grunt/if.js
+++ config/grunt/if.js
@@ -7,7 +7,7 @@ module.exports = {
             test: () => process.env.TRAVIS === 'true' &&
                     process.env.TRAVIS_PULL_REQUEST === 'false' &&
                     process.env.TRAVIS_SECURE_ENV_VARS === 'true' &&
-                    process.env.TRAVIS_TAG !== 'false'
+                    process.env.TRAVIS_TAG !== undefined
         }
     }
 };

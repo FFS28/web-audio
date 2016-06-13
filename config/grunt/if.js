959c74b023b8e424032dc158b406eb8ae8051a6a
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-13 02:38:46 +0200

    fix syntax error

--- config/grunt/if.js
+++ config/grunt/if.js
@@ -11,6 +11,7 @@ module.exports = {
                     process.env.TRAVIS_PULL_REQUEST === 'false' &&
                     process.env.TRAVIS_SECURE_ENV_VARS === 'true' &&
                     process.env.TRAVIS_TAG !== undefined);
+            }
         }
     }
 };

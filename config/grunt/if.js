070a2a8fa8e2c03a97effc26c9388deb30cfda1f
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-13 02:31:52 +0200

    temporary log TRAVIS_TAG

--- config/grunt/if.js
+++ config/grunt/if.js
@@ -4,10 +4,13 @@ module.exports = {
     deploy: {
         ifTrue: [ 'gh-pages:deploy' ],
         options: {
-            test: () => process.env.TRAVIS === 'true' &&
+            test: () => {
+                console.log(process.env.TRAVIS_TAG);
+
+                return (process.env.TRAVIS === 'true' &&
                     process.env.TRAVIS_PULL_REQUEST === 'false' &&
                     process.env.TRAVIS_SECURE_ENV_VARS === 'true' &&
-                    process.env.TRAVIS_TAG !== undefined
+                    process.env.TRAVIS_TAG !== undefined);
         }
     }
 };

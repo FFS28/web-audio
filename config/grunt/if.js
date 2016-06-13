f80a88ac1931613d41ccdc625c7c14850c60ce1f
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-13 02:49:01 +0200

    remove temporary log

--- config/grunt/if.js
+++ config/grunt/if.js
@@ -4,14 +4,10 @@ module.exports = {
     deploy: {
         ifTrue: [ 'gh-pages:deploy' ],
         options: {
-            test: () => {
-                console.log('TRAVIS_TAG: "' + process.env.TRAVIS_TAG + '"');
-
-                return (process.env.TRAVIS === 'true' &&
+            test: () => process.env.TRAVIS === 'true' &&
                     process.env.TRAVIS_PULL_REQUEST === 'false' &&
                     process.env.TRAVIS_SECURE_ENV_VARS === 'true' &&
-                    process.env.TRAVIS_TAG !== '');
-            }
+                    process.env.TRAVIS_TAG !== ''
         }
     }
 };

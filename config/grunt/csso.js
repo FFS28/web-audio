5f4ac6d38cc62d94994dd0cd069a72db71efc46c
chrisguttandin <chrisguttandin@media-codings.com>
2016-07-04 18:54:34 +0200

    fix css minification

--- config/grunt/csso.js
+++ config/grunt/csso.js
@@ -7,6 +7,9 @@ module.exports = {
             dest: 'build/',
             expand: true,
             src: [ '**/*.css' ]
+        }, {
+            dest: 'build/styles/prism-okaidia.css',
+            src: 'node_modules/prismjs/themes/prism-okaidia.css'
         } ]
     }
 };

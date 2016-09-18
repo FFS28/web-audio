b799f659d2bd467dd8d5f37bdc89dc462387a840
chrisguttandin <chrisguttandin@media-codings.com>
2016-09-18 11:33:34 +0200

    remove unnecessary systemjs config

--- src/app/system-development.js
+++ src/app/system-development.js
@@ -4,27 +4,21 @@ System.config({
     },
     packages: {
         '@angular/common': {
-            defaultExtension: 'js',
             main: 'bundles/common.umd.js'
         },
         '@angular/compiler': {
-            defaultExtension: 'js',
             main: 'bundles/compiler.umd.js'
         },
         '@angular/core': {
-            defaultExtension: 'js',
             main: 'bundles/core.umd.js'
         },
         '@angular/platform-browser': {
-            defaultExtension: 'js',
             main: 'bundles/platform-browser.umd.js'
         },
         '@angular/platform-browser-dynamic': {
-            defaultExtension: 'js',
             main: 'bundles/platform-browser-dynamic.umd.js'
         },
         '@angular/router': {
-            defaultExtension: 'js',
             main: 'bundles/router.umd.js'
         },
         'app': {

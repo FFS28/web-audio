35f8d22eed47429c060a003fa49e80a1ff43a560
chrisguttandin <chrisguttandin@media-codings.com>
2016-09-19 00:02:20 +0200

    apply linting rules

--- src/app/modules/app/module.ts
+++ src/app/modules/app/module.ts
@@ -1,6 +1,6 @@
 import { SlidesModule } from '../slides/module';
-import { appRoutes } from './routes';
 import { AppComponent } from './components/app/component';
+import { appRoutes } from './routes';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';

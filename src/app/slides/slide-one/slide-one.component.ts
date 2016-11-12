ad7a663154acc2cc60dc07d0f98feaff1a995116
chrisguttandin <chrisguttandin@media-codings.com>
2016-11-12 13:37:29 +0100

    apply linting rules

--- src/app/slides/slide-one/slide-one.component.ts
+++ src/app/slides/slide-one/slide-one.component.ts
@@ -3,6 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrls: [ './slide-one.component.css' ],
-    templateUrl: './slide-one.component.html',
+    templateUrl: './slide-one.component.html'
 })
 export class SlideOneComponent { }

751dc4a5ef9d758159bfc7f8f4b6b36b22f0b4e8
chrisguttandin <chrisguttandin@media-codings.com>
2016-12-10 10:39:06 +0100

    update grunt-depcheck

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -4,8 +4,22 @@ module.exports = {
     default: {
         options: {
             failOnUnusedDeps: true,
-            ignoreMatches: [ 'hammerjs', 'prismjs' ],
-            withoutDev: true
+            ignoreMatches: [
+                'hammerjs',
+                'prismjs',
+                '@angular/compiler-cli',
+                '@types/*',
+                'angular-cli',
+                'babel-eslint',
+                'eslint-config-holy-grail',
+                'grunt-*',
+                'husky',
+                'jasmine',
+                'karma*',
+                'tslint',
+                'tslint-config-holy-grail',
+                'typescript'
+            ]
         },
         src: './'
     }

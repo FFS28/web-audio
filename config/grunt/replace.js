bffc5b3f4967fadeb8eeb81e0ff3a313a8d8b623
chrisguttandin <chrisguttandin@media-codings.com>
2017-06-15 16:10:58 +0200

    apply linting rules

--- config/grunt/replace.js
+++ config/grunt/replace.js
@@ -1,8 +1,7 @@
 const cspBuilder = require('content-security-policy-builder');
+const cspProductionConfig = require('../csp/production');
 const crypto = require('crypto');
 const fs = require('fs');
-
-const cspProductionConfig = require('../csp/production');
 
 module.exports = {
     'chunks': {
@@ -29,9 +28,7 @@ module.exports = {
                 match: /<meta\shttp-equiv="content-security-policy">/,
                 replacement: () => {
                     const html = fs.readFileSync('build/index.html', 'utf-8');
-
                     const regex = /<script[^>]*?>([^<](.|[\n\r])*?)<\/script>/gm;
-
                     const scriptHashes = [];
 
                     let result = regex.exec(html);
@@ -54,7 +51,6 @@ module.exports = {
                                 : [ ...scriptHashes ]
                         })
                     });
-
                     const cspString = cspBuilder(cspConfig);
 
                     return `<meta content="${ cspString }" http-equiv="content-security-policy">`;

7059ef3ff0c711eeb63cea0dbc9f56b865cc12a1
chrisguttandin <chrisguttandin@media-codings.com>
2017-09-03 12:51:43 +0200

    chore(config): update depcheck config

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -6,6 +6,7 @@ module.exports = {
                 'build'
             ],
             ignoreMatches: [
+                '@angular/service-worker',
                 'hammerjs',
                 'prismjs',
                 'tslib',

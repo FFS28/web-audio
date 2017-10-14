3122ccb790f81da88d0bb8824c439a4981ec3df5
Christoph Guttandin <chrisguttandin@media-codings.com>
2017-10-14 04:50:06 +0200

    chore(config): update grunt config

--- config/grunt/depcheck.js
+++ config/grunt/depcheck.js
@@ -24,6 +24,7 @@ module.exports = {
                 'husky',
                 'jasmine-core',
                 'karma*',
+                'stylelint-config-holy-grail',
                 'tsconfig-holy-grail',
                 'tslint',
                 'tslint-config-holy-grail',

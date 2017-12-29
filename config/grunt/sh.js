a2223f6ec3aa840b909e3af5a5a977f4c4df5ee7
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-29 19:15:53 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,9 +1,9 @@
 module.exports = {
     analyze: {
-        cmd: 'ng build --aot --build-optimizer false --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
+        cmd: 'ng build --aot --build-optimizer --prod --sourcemaps --stats-json && (bundle-buddy build/*.map & webpack-bundle-analyzer build/stats.json)'
     },
     build: {
-        cmd: "ng build --aot --base-href /web-audio-conference-2016 --build-optimizer false --no-sourcemap --prod --stats-json && webpack-stats-duplicates build/stats.json --whitelist 'multi ./node_modules/prismjs' && rm build/stats.json"
+        cmd: "ng build --aot --base-href /web-audio-conference-2016 --build-optimizer --no-sourcemaps --prod --stats-json && webpack-stats-duplicates build/stats.json --whitelist 'multi ./node_modules/prismjs' && rm build/stats.json"
     },
     continuous: {
         cmd: 'ng test'
@@ -15,10 +15,10 @@ module.exports = {
         cmd: 'ng lint --type-check'
     },
     monitor: {
-        cmd: 'ng serve --port 9955'
+        cmd: 'ng serve --aot --port 9955'
     },
     preview: {
-        cmd: 'ng serve --aot --port 9955 --prod'
+        cmd: 'ng serve --aot --build-optimizer --port 9955 --prod'
     },
     smoke: {
         cmd: 'IS_SMOKE_TEST=true ng e2e --serve false && sonarwhal https://chrisguttandin.github.io/web-audio-conference-2016 && rm cdp.pid'

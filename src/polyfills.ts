aa6d904a51ea167b1e84e8169a51eddfa938b1b9
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-10-21 01:30:47 +0200

    fix: remove reflect polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,1 @@
-/*
- * @todo The reflect polyfill is currently only needed to run the e2e tests. For some reason the e2e tests do not use the AoT compiler and
- * therefore need the polyfill.
- */
-import 'core-js/es7/reflect'; // tslint:disable-line:no-submodule-imports
 import 'zone.js/dist/zone';

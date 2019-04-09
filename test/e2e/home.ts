7c0ecddac35e1ac98892d2a5cd6920fc11295c7c
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-04-09 14:19:34 +0200

    test: update e2e test

--- test/e2e/home.ts
+++ test/e2e/home.ts
@@ -38,6 +38,12 @@ describe('/', () => {
 
         element(by.tagName('body')).sendKeys(Key.ARROW_RIGHT);
 
+        /*
+         * @todo Unfortunately an arbitrary call browser.sleep() is used here as both element(by.tagName('body')).allowAnimations(false)
+         * and browser.waitForAngular() have no effect.
+         */
+        browser.sleep(1000);
+
         expect(page.getSubHeadline()).toEqual('About me');
     });
 

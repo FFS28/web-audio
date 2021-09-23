f90905635a701efa364e9f703bd3f750728b2b20
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-09-23 23:24:03 +0200

    refactor: use destructuring to define the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
-        loadChildren: () => import('./slides').then((mdl) => mdl.SlidesModule),
+        loadChildren: () => import('./slides').then(({ SlidesModule }) => SlidesModule),
         path: 'slides'
     },
     {
